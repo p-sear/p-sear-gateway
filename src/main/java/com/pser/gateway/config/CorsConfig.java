9b64471b479116f4801f58a409d8351d53b1f118
amanecopse <amanecopse@gmail.com>
2024-05-21 14:11:53 +0900

    feat: Cors 설정 수정

--- src/main/java/com/pser/gateway/config/CorsConfig.java
+++ src/main/java/com/pser/gateway/config/CorsConfig.java
@@ -1,6 +1,10 @@
 package com.pser.gateway.config;
 
+import com.pser.gateway.config.CorsConfig.CorsProperties;
 import java.util.List;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,20 +12,30 @@ import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableConfigurationProperties(CorsProperties.class)
 public class CorsConfig {
 
     @Bean("corsConfigurationSource")
-    CorsConfigurationSource corsConfigurationSource() {
-        List<String> allowedOrigins = List.of("http://localhost:3000");
+    CorsConfigurationSource corsConfigurationSource(CorsProperties corsProperties) {
         List<String> allowedMethods = List.of("*");
         List<String> allowedHeaders = List.of("*");
 
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedOrigins(corsProperties.getOrigins());
         configuration.setAllowedMethods(allowedMethods);
         configuration.setAllowedHeaders(allowedHeaders);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Getter
+    @ConfigurationProperties("spring.security.cors")
+    public static class CorsProperties {
+        private final List<String> origins;
+
+        public CorsProperties(List<String> origins) {
+            this.origins = origins;
+        }
     }
 }
